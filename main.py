5545178dd90720f4d5ab0b1d1437951252eef2ad
Carter Hinsley <carterhinsley@outlook.com>
2021-05-17 07:37:12 +0000

    Add move validation

--- main.py
+++ main.py
@@ -66,9 +66,49 @@ def handle_command(cursor, command):
         cursor = (int(words[1]), int(words[2]))
     elif words[0] == "place":
         stone_pos = cursor if len(words) == 1 else (int(words[1]), int(words[2]))
+        
+        # Check placement criteria.
+        valid_move = True
+
+        # Check that the stone is being placed in an empty location.
         if stone_pos in board:
+            valid_move = False
             print(f"You cannot place a stone at {stone_pos}, as there is another already present.")
-        else:
+
+        # Iterate over the local region, checking the relevant locking and existence criteria.
+        stone_in_region = False
+        locked_stones = 0
+        for row in range(-LOCAL_REGION_SIZE // 2 + 1, LOCAL_REGION_SIZE // 2 + 1):
+            for col in range(-LOCAL_REGION_SIZE // 2 + 1, LOCAL_REGION_SIZE // 2 + 1):
+                coords = (stone_pos[0] + row, stone_pos[1] + col)
+                if coords in board:
+                    stone_in_region = True
+                    # Check if the stone is locked.
+                    if board[coords]["status"] == "locked":
+                        locked_stones += 1
+                        # Check if the locked stone belongs to the player.
+                        if board[coords]["player"] == "You":
+                            valid_move = False
+                            print(f"You cannot place a stone at {stone_pos}, as you have a locked stone at {coords}.")
+                        # Check that the locked stone threshold has not been met.
+                        if locked_stones > 1:
+                            valid_move = False
+                            print(f"You cannot place a stone at {stone_pos}, as there are multiple locked stones in the local region.")
+                    # Check if the stone is self-locked.
+                    elif board[coords]["status"] == "self-locked":
+                        # Check if the self-locked stone belongs to the player.
+                        player = board[coords]["player"]
+                        if player != "You":
+                            valid_move = False
+                            print(f"You cannot place a stone at {stone_pos}, as there is a self-locked stone at {coords} belonging to {player}.")
+
+        # Check whether there is another stone in the local region.
+        if not stone_in_region:
+            valid_move = False
+            print(f"You cannot place a stone at {stone_pos}, as there is no other stone present in the local region.")
+
+        # If valid, place stone.
+        if valid_move:
             board[stone_pos] = {
                 "player": "You",
                 "status": "locked",
