f167bd3704d9729f4d9b582f6b150f0b6277f4fc
Carter Hinsley <carterhinsley@outlook.com>
2021-05-17 06:40:07 +0000

    Add cursor movement and stone placement

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 LOCAL_REGION_SIZE = 13
 
+cursor = (0, 0)
+
 board = {
     (0, 0): {
         "player": "GOD",
@@ -44,5 +46,34 @@ def draw(board, cursor_coords):
         drawing += "-\n"
     print(drawing)
 
+def handle_command(cursor, command):
+    # TODO: Yes, I know that this control flow is unbecoming. I
+    # will make a separate module for this later.
+    if command == "up":
+        cursor = (cursor[0], cursor[1] - 1)
+    elif command == "right":
+        cursor = (cursor[0] + 1, cursor[1])
+    elif command == "down":
+        cursor = (cursor[0], cursor[1] + 1)
+    elif command == "left":
+        cursor = (cursor[0] - 1, cursor[1])
+    elif command == "go":
+        if cursor in board:
+            print("You cannot place a stone there, as there is another already present.")
+        else:
+            board[cursor] = {
+                "player": "You",
+                "status": "locked",
+            }
+
+    return cursor
+
 if __name__ == "__main__":
-    draw(board, (0, 0))
+    while True:
+        draw(board, cursor)
+        print(cursor)
+        try:
+            print(board[cursor])
+        except KeyError:
+            pass
+        cursor = handle_command(cursor, input("> ").strip())
