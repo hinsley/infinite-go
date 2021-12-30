5b1e3fc0d788225d923254010ba2708054e248c3
Carter Hinsley <carterhinsley@outlook.com>
2021-12-30 08:15:40 +0000

    Add pan, zoom

--- static/scripts/render_goban_canvas.js
+++ static/scripts/render_goban_canvas.js
@@ -1,28 +1,135 @@
-const rulingSpacing = 50; // Pixels.
-const rulingWidth = 2; // Pixels.
-const hoshiRadius = 2; // Pixels.
+var rulingSpacing = 50; // Pixels (fixed at 1 in world units).
+var rulingWidth = 0.01; // World units.
+const hoshiRadius = 0.1; // World units.
+const drawRate = 60; // Hz.
+const zoomSpeed = 3; // Pixels per scroll unit.
 
 const canvas = document.getElementById("goban");
 const ctx = canvas.getContext("2d");
 
-var x = Math.round(rulingSpacing / 2);
-var y = Math.round(rulingSpacing / 2);
+var _x = -0.5;
+var _y = -0.5;
+// var _x = Math.round(rulingSpacing / 2);
+// var _y = Math.round(rulingSpacing / 2);
+
+// Pixel coordinates.
+var _x_offset = 0;
+var _y_offset = 0;
+
+// Pixel coordinates.
+var cursor_x_initial = null;
+var cursor_y_initial = null;
+
+var panning = false;
+
+canvas.addEventListener("mousedown", (e) => {
+    console.log(canvas2World(mouseX(e), mouseY(e)));
+    // Start cursor tracker.
+    cursor_x_initial = mouseX(e);
+    cursor_y_initial = mouseY(e);
+    panning = true;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    if (panning) {
+        _x_offset = cursor_x_initial - mouseX(e);
+        _y_offset = cursor_y_initial - mouseY(e);
+    }
+});
+
+["mouseup", "mouseleave"].forEach((event_name) => {
+    canvas.addEventListener(event_name, () => {
+        _x += _x_offset / rulingSpacing;
+        _y += _y_offset / rulingSpacing;
+        _x_offset = 0;
+        _y_offset = 0;
+        panning = false;
+    });
+});
+
+// Zoom event.
+canvas.addEventListener("wheel", (e) => {
+    // Zoom around the cursor.
+    // Store the current cursor position in world coordinates.
+    var cursor_world_coords = canvas2World(mouseX(e), mouseY(e));
+    rulingSpacing = clamp(rulingSpacing - (e.deltaY / Math.abs(e.deltaY)) * zoomSpeed, 5, 50);
+    // Move the viewport to restore the cursor position.
+    var new_cursor_world_coords = canvas2World(mouseX(e), mouseY(e));
+    _x += cursor_world_coords[0] - new_cursor_world_coords[0];
+    _y += cursor_world_coords[1] - new_cursor_world_coords[1];
+});
+
+
+function x() {
+    // Returns the x coordinate of the viewport in world coordinates.
+    return _x + _x_offset / rulingSpacing;
+}
+
+function y() {
+    // Returns the y coordinate of the viewport in world coordinates.
+    return _y + _y_offset / rulingSpacing;
+}
+
+function mouseX(e) {
+    // Returns the x coordinate of the mouse in canvas coordinates (relative to top left corner).
+    return e.clientX - canvas.getBoundingClientRect().left;
+}
+
+function mouseY(e) {
+    // Returns the y coordinate of the mouse in canvas coordinates (relative to top left corner).
+    return e.clientY - canvas.getBoundingClientRect().top;
+}
+
+function clamp(val, min, max) {
+    return Math.min(Math.max(val, min), max);
+}
 
 function drawRulings() {
     ctx.fillStyle = "black";
 
+    current_x = x();
+    current_y = y();
+
     // Draw vertical rulings.
-    for (var i = 0; i < 13; i++) {
-        ctx.fillRect((Math.ceil(x / rulingSpacing) + i) * rulingSpacing - x, 0, 1, canvas.height);
+    for (var i = 0; i < canvas.width / rulingSpacing; i++) {
+        ctx.fillRect(
+            (Math.ceil(current_x) - current_x + i) * rulingSpacing,
+            0,
+            rulingWidth * rulingSpacing,
+            canvas.height
+        );
     }
     // Draw horizontal rulings.
-    for (var i = 0; i < 13; i++) {
-        ctx.fillRect(0, (Math.ceil(y / rulingSpacing) + i) * rulingSpacing - y, canvas.width, 1);
+    for (var i = 0; i < canvas.height / rulingSpacing; i++) {
+        ctx.fillRect(
+            0,
+            (Math.floor(current_y) - current_y + i + 1) * rulingSpacing,
+            canvas.width,
+            rulingWidth * rulingSpacing
+        );
     }
+
+    // TODO: Draw hoshi.
 }
 
-function world2Viewport(x, y) {
-    return [x - this.x, y - this.y];
+function world2Canvas(world_x, world_y) {
+    world_canvas_coords = canvas2World(0, 0);
+    return [
+        (world_x - x()) * rulingSpacing,
+        (world_y - y()) * rulingSpacing
+    ];
 }
 
-drawRulings();
+function canvas2World(canvas_x, canvas_y) {
+    return [x() + canvas_x / rulingSpacing, y() + canvas_y / rulingSpacing];
+}
+
+// Draw the image on the canvas.
+setInterval(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawRulings();
+    // TODO: Erase.
+    // Reference for the origin.
+    // origin_coords = world2Canvas(0, 0);
+    // ctx.fillRect(origin_coords[0] - 5, origin_coords[1] - 5, 10, 10);
+}, 1/drawRate);
